34a629cb8472c4bf917df1643e117c7c501a3a74
Spencer Lewis <spenc.lewis@gmail.com>
2020-08-25 12:00:31 -0600

    Adding Exercise Files

---
+++ Week-1/1-Use-PHP-Tags-Solution.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Use PHP Tags Exercise</title>
+</head>
+
+<body>
+    <?php
+    echo "This is in PHP tags.";
+    ?>
+</body>
+
+</html>
