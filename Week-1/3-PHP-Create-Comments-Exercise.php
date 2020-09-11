b626aced9980ecf755e8e061f9384d4c17cd66a4
Spencer Lewis <spenc.lewis@gmail.com>
2020-09-11 14:51:01 -0600

    Adding exercise files

---
+++ Week-1/3-PHP-Create-Comments-Exercise.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Creating Comments in PHP Exercise</title>
+</head>
+
+<body>
+    <?php
+
+    Make this a single line comment.
+
+    Make this a
+    multi-line comment.
+    And then make a new
+    comment of your own below.
+
+
+    ?>
+</body>
+
+</html>
---
+++ Week-1/3-PHP-Create-Comments-Solution.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Creating Comments in PHP Solution</title>
+</head>
+
+<body>
+    <?php
+
+    //Make this a single line comment.
+
+    /*Make this a
+        mult-iline comment.
+        And then make a new
+        comment of your own below.*/
+
+    //This is my own comment.
+
+    ?>
+</body>
+
+</html>
