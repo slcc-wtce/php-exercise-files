8a85334e6aa55ad8e82885fb732241b6410cce3a
Spencer Lewis <spenc.lewis@gmail.com>
2020-09-14 11:50:24 -0600

    Adding exercise files

---
+++ Week-1/5-PHP-Data-Types-Exercise.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Most Common PHP Data Types Exercise</title>
+</head>
+
+<body>
+    <!--Follow the instructions below to create variables with different data types-->
+    <?php
+    //Create a variable that contains an integer
+
+
+    //Create a variable that contains a float
+
+
+    //Create a variable that contains a string
+
+
+    //Create a variable that contains a Boolean value
+
+
+    //Create a variable that contains an array
+
+
+    //Create an echo statement top send each variable back to the screen to see the content
+
+
+    ?>
+</body>
+
+</html>
---
+++ Week-1/5-PHP-Data-Types-Solution.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Most Common PHP Data Types Solution</title>
+</head>
+
+<body>
+    <!--Follow the instructions below to create variables with different data types-->
+    <?php
+    //Create a variable that contains an integer
+    $varInt = 2;
+
+    //Create a variable that contains a float
+    $varFloat = 1.2;
+
+    //Create a variable that contains a string
+    $varStr = "This is a string of text.";
+
+    //Create a variable that contains a Boolean value
+    $varBool = true;
+
+    //Create a variable that contains an array
+    $varArray = ['Red', 'Green', 'Blue'];
+
+    //Create an echo statement top send each variable back to the screen to see the content
+    echo $varInt;
+    // echo $varFloat;
+    // echo $varStr;
+    // echo $varBool;
+    // print_r ($varArray); //This will print the entire array to the screen with their indexes.
+
+    ?>
+</body>
+
+</html>
