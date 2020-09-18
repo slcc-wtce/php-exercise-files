52c7e499bb15968c0a8e89bda398255f735dd86f
Spencer Lewis <spenc.lewis@gmail.com>
2020-09-18 14:11:44 -0600

    Adding exercise files

---
+++ Week-1/21-PHP-For-Loop-Exercise.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Using a For loop in PHP Exercise</title>
+</head>
+
+<body>
+    <?php
+    /**
+     * 1. Create a basic for loop
+     * 2. In the condition, 
+     *    a. Create a variable and set the value to 1
+     *    b. Test if the variable is less than 10
+     *    c. Increment the variable up by one
+     * 3. In the code block to be run, do the following 
+     *    a. Create an echo statement that sends the string "The number is: (the value of the variable)"
+     *    b. Add a line break
+     */
+
+    ?>
+</body>
+
+</html>
