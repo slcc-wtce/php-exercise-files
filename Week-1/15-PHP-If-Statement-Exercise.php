6d798d38cc71bd698485de7fc0393596d9762635
Spencer Lewis <spenc.lewis@gmail.com>
2020-09-18 09:35:40 -0600

    Adding exercise files

---
+++ Week-1/15-PHP-If-Statement-Exercise.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Using Conditional If Statements in PHP Exercise</title>
+</head>
+
+<body>
+    <?PHP
+    //Create a variable that is assigned an integer
+
+
+    /******************************************************
+     * 1. Create a basic if statement
+     * 2. Test if the variable is equal to what it is currently set to
+     * 3. Create an echo statement that sends "This is TRUE!" within curly brackets
+     * 4. Try changing the values of the variable and the test statement to see when the code runs
+     ******************************************************/
+
+    ?>
+</body>
+
+</html>
---
+++ Week-1/15-PHP-If-Statement-Solution.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Using Conditional If Statements in PHP Solution</title>
+</head>
+
+<body>
+    <?PHP
+    //Create a variable that is assigned an integer
+    $x = 3;
+
+    /******************************************************
+     * 1. Create a basic if statement
+     * 2. Test if the variable is equal to what it is currently set to
+     * 3. Create an echo statement that sends "This is TRUE!" within curly brackets
+     * 4. Try changing the values of the variable and the test statement to see when the code runs
+     ******************************************************/
+    if ($x = 3) {
+        echo "This is TRUE!";
+    }
+
+    ?>
+</body>
+
+</html>
