93d15fed5d61bc02cbb77f1ff0668c91ff89002b
Spencer Lewis <spenc.lewis@gmail.com>
2020-09-18 11:12:27 -0600

    Adding exercise files

---
+++ Week-1/17-PHP-If-Elseif-Else-Statement-Exercise.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Using PHP If, Elseif, Else Statement Exercise</title>
+</head>
+
+<body>
+    <?php
+    //Create a variable that is assigned an integer between 1 and 10
+
+
+    /******************************************************
+     * 1. Create a basic if statement
+     * 2. Set the first criteria to test if the variable is equal to 5
+     * 3. Create an echo statement that sends "The variable is 5." within curly brackets after the condition
+     * 4. Add the elseif clause
+     * 5. Set the second criteria to test if the variable is greater than 5
+     * 6. Create an echo statement that sends "The variable is greater than 5." within curly brackets after the elseif clause
+     * 7. Add the else clause
+     * 8. Create an echo statement that sends "Variable is less than 5." within curly brackets after the else clause
+     * 9. Try changing the values of the variable and the test statement to see when the code runs
+     ******************************************************/
+
+    ?>
+</body>
+
+</html>
