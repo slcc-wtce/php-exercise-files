c34bf1dcd648f4468054af60aa7d0a871cf20d0e
Spencer Lewis <spenc.lewis@gmail.com>
2020-09-18 13:54:43 -0600

    Adding exercise files

---
+++ Week-1/19-PHP-While-Loop-Solution.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Using a While loop in PHP Solution</title>
+</head>
+
+<body>
+    <?php
+    //Create a variable that is assigned the integer value of 1
+    $x = 1;
+    /**
+     * 1. Create a basic while loop
+     * 2. In the condition, test if the variable is less than 10
+     * 3. In the code block to be run, do the following 
+     *    a. Create an echo statement that sends the string "The number is: (the value of the variable)"
+     *    b. Add a line break
+     *    c. Increment the value assigned to the variable up by one 
+     */
+    while ($x < 10) {
+        echo "The number is: $x" . "<br>";
+        $x++;
+    }
+    ?>
+</body>
+
+</html>
