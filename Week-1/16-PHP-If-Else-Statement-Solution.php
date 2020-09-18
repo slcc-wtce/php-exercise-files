b42132dd892339a78218d0927ad48f8268b62aaa
Spencer Lewis <spenc.lewis@gmail.com>
2020-09-18 10:42:31 -0600

    Added exercise files

--- Week-1/16-PHP-If-Else-Statement-Solution.php
+++ Week-1/16-PHP-If-Else-Statement-Solution.php
@@ -4,24 +4,27 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Using Conditional If Statements in PHP Solution</title>
+    <title>Using If, Else Statements in PHP Solution</title>
 </head>
 
 <body>
-    <?PHP
+    <?php
     //Create a variable that is assigned an integer
     $x = 3;
 
     /******************************************************
      * 1. Create a basic if statement
      * 2. Test if the variable is equal to what it is currently set to
-     * 3. Create an echo statement that sends "This is TRUE!" within curly brackets
-     * 4. Try changing the values of the variable and the test statement to see when the code runs
+     * 3. Create an echo statement that sends "This is TRUE!" within curly brackets after the condition
+     * 4. Add the else clause
+     * 5. Create an echo statement that sends "This is FALSE!" within curly brackets after the else clause
+     * 6. Try changing the values of the variable and the test statement to see when the code runs
      ******************************************************/
     if ($x == 3) {
         echo "This is TRUE!";
+    } else {
+        echo "This is FALSE!";
     }
-
     ?>
 </body>
 
