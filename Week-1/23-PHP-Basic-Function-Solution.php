1933c5a15175b28277f24b862d4fd97864249527
Spencer Lewis <spenc.lewis@gmail.com>
2020-09-21 13:59:02 -0600

    Adding exercise files

---
+++ Week-1/23-PHP-Basic-Function-Solution.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Creating a Function in PHP Solution</title>
+</head>
+
+<body>
+    <?php
+    /**
+     * 1. Create a function named newMsg
+     * 2. In the code block, create an echo statement that will send "Hello World" to the screen
+     */
+    function newMsg()
+    {
+        echo "Hello World!";
+    }
+
+    //Call the function using its name followed by a opening and closing parenthesis
+    newMsg();
+
+    ?>
+</body>
+
+</html>
