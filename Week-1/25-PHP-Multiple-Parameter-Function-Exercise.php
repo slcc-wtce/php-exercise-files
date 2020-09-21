bb40ca06d5912617e096cd5e92978a30e510f446
Spencer Lewis <spenc.lewis@gmail.com>
2020-09-21 14:31:12 -0600

    Adding exercise files

---
+++ Week-1/25-PHP-Multiple-Parameter-Function-Exercise.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Creating a Function with a Single Parameter in PHP Exercise</title>
+</head>
+
+<body>
+    <?php
+    /**
+     * 1. Create a function named newGreeting
+     * 2. Within the parentheses, add the variable $name and $day as the parameters to pass into the code block
+     * 2. In the code block, create an echo statement that will send 
+     *    "<h1>Welcome $name!</h1><p>It's a beautiful $day!</p>" to the screen
+     */
+
+
+    /**
+     * 1. Call the function using its name followed by a opening and closing parentheses
+     * 2. In the parentheses:
+     *    a. Add a name as a string for the first parameter followed by a comma
+     *    b. For the second parameter, add the date function that will get the name of the day: date("l")'
+     */
+
+
+    ?>
+</body>
+
+</html>
