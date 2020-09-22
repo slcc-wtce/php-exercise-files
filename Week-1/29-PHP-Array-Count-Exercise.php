504f051df7af64b1de4caad5cb25836fb1e646b7
Spencer Lewis <spenc.lewis@gmail.com>
2020-09-22 13:31:07 -0600

    Added exercise files

---
+++ Week-1/29-PHP-Array-Count-Exercise.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Using Indexed Arrays in PHP Exercise</title>
+</head>
+<body>
+<?php
+    //Use this array for this exercise
+    $colors = array("Red", "Green", "Orange", "Blue", "Pink", "Red Orange", "Light Blue", "Orange Green", "Sky Blue", "Purple");
+
+    /**
+     * Create an echo statement that counts the number of items
+     * contained in the $colors array and outputs the sentence
+     * "There are (number of items) colors in the array."
+	*/
+
+    
+?>
+</body>
+</html>
