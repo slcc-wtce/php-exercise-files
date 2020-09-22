562af48a5c1516254a4671d6ad7a20aaa0c7f584
Spencer Lewis <spenc.lewis@gmail.com>
2020-09-22 17:52:12 -0600

    Adding exercise files

--- Week-1/31-PHP-Globals-Example.php
+++ Week-1/31-PHP-Globals-Example.php
@@ -20,9 +20,11 @@
     $x = 75; 
     $y = 25;
     function addition() { 
-    $GLOBALS['z'] = $GLOBALS['x'] + $GLOBALS['y']; 
-    }
+            $GLOBALS['z'] = $GLOBALS['x'] + $GLOBALS['y']; 
+        }
+    
     addition(); 
+    
     echo "$x + $y = $z"; 
     ?>
     
