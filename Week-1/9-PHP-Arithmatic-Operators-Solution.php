8234215edbb1143a2b8d9a52ebfa53750b120373
Spencer Lewis <spenc.lewis@gmail.com>
2020-10-13 16:54:06 -0600

    Updated for conformity

--- Week-1/9-PHP-Arithmatic-Operators-Solution.php
+++ Week-1/9-PHP-Arithmatic-Operators-Solution.php
@@ -10,51 +10,71 @@
 <body>
     <?php
 
-    //Create two variables and assign them an integer or a float
-    $x = 6;
-    $y = 3;
+        //Create two variables and assign them an integer or a float
+        $x = 6;
+        $y = 3;
 
-    /*1. Create a variables that adds the earlier two
-	2. Echo the variable to the screen
-	3. Create an echo statement that sends a line break to the screen*/
-    $z = $x + $y;
-    echo "$x + $y = $z";
-    echo "<br>";
+        /**
+         * 1. Create a variables that adds the earlier two
+         * 2. Echo the variable to the screen
+         * 3. Create an echo statement that sends a 
+         *    line break to the screen
+         **/
+        $z = $x + $y;
+        echo "$x + $y = $z";
+        echo "<br>";
 
-    /*1. Create a variables that subtracts the earlier two
-	2. Echo the variable to the screen
-	3. Create an echo statement that sends a line break to the screen*/
-    $z = $x - $y;
-    echo "$x - $y = $z";
-    echo "<br>";
+        /**
+         * 1. Create a variables that subtracts the earlier two
+         * 2. Echo the variable to the screen
+         * 3. Create an echo statement that sends a 
+         *    line break to the screen
+         **/
+        $z = $x - $y;
+        echo "$x - $y = $z";
+        echo "<br>";
 
-    /*1. Create a variables that calculates the modulus (remainder) the earlier two
-	2. Echo the variable to the screen
-	3. Create an echo statement that sends a line break to the screen*/
-    $z = $x % $y;
-    echo "$x % $y = $z";
-    echo "<br>";
+        /**
+         * 1. Create a variables that calculates the 
+         *    modulus (remainder) the earlier two
+         * 2. Echo the variable to the screen
+         * 3. Create an echo statement that sends a 
+         *    line break to the screen
+         **/
+        $z = $x % $y;
+        echo "$x % $y = $z";
+        echo "<br>";
 
-    /*1. Create a variables that divides the earlier two
-	2. Echo the variable to the screen
-	3. Create an echo statement that sends a line break to the screen*/
-    $z = $x / $y;
-    echo "$x / $y = $z";
-    echo "<br>";
+        /**
+         * 1. Create a variables that divides the earlier two
+         * 2. Echo the variable to the screen
+         * 3. Create an echo statement that sends a 
+         *    line break to the screen
+         **/
+        $z = $x / $y;
+        echo "$x / $y = $z";
+        echo "<br>";
 
-    /*1. Create a variables that multiplies the earlier two
-	2. Echo the variable to the screen
-	3. Create an echo statement that sends a line break to the screen*/
-    $z = $x * $y;
-    echo "$x * $y = $z";
-    echo "<br>";
+        /**
+         * 1. Create a variables that multiplies the earlier two
+         * 2. Echo the variable to the screen
+         * 3. Create an echo statement that sends a 
+         *    line break to the screen
+         **/
+        $z = $x * $y;
+        echo "$x * $y = $z";
+        echo "<br>";
 
-    /*1. Create a variables that calculates the first to the power of the second.
-	2. Echo the variable to the screen
-	3. Create an echo statement that sends a line break to the screen*/
-    $z = $x ** $y;
-    echo "$x ** $y = $z";
-    echo "<br>";
+        /**
+         * 1. Create a variables that calculates the first 
+         *    to the power of the second.
+         * 2. Echo the variable to the screen
+         * 3. Create an echo statement that sends a 
+         *    line break to the screen
+         **/
+        $z = $x ** $y;
+        echo "$x ** $y = $z";
+        echo "<br>";
 
     ?>
 </body>
