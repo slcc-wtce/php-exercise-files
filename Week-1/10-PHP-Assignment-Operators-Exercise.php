8c5c1871486f05af2bfaa072c2a38cdfe9119e27
Spencer Lewis <spenc.lewis@gmail.com>
2020-10-13 16:04:49 -0600

    Updated to match exercise

--- Week-1/10-PHP-Assignment-Operators-Exercise.php
+++ Week-1/10-PHP-Assignment-Operators-Exercise.php
@@ -9,33 +9,43 @@
 
 <body>
     <?php
-    //Create at least two variables and assign them an integer or float
-
-
-    /*1. Create an echo statement to show the results of assigning the value of the second variable to the first
-        2. Create an echo statement that sends a line break to the screen.*/
-
-
-    /*1. Create an echo statement to show the results of adding the value of the second variable to the first
-        2. Create an echo statement that sends a line break to the screen.*/
-
-
-    /*1. Create an echo statement to show the results of subtracting the value of the second variable to the first
-        2. Create an echo statement that sends a line break to the screen.*/
-
-
-    /*1. Create an echo statement to show the results of multiplying the value of the second variable to the first
-        2. Create an echo statement that sends a line break to the screen.*/
-
-
-    /*1. Create an echo statement to show the results of dividing the value of the second variable to the first
-        2. Create an echo statement that sends a line break to the screen.*/
-
-
-    /*1. Create an echo statement to show the modulus (remainder) between the second variable to the first
-        2. Create an echo statement that sends a line break to the screen.*/
-
-
+        //Create at least two variables and assign them an integer or float
+        
+        /**
+         * 1. Create an echo statement to show the results
+         *    of assigning the value of the second variable to the first
+         * 2. Create an echo statement that sends a line break to the screen.
+         **/
+        
+        /**
+         * 1. Create an echo statement to show the results 
+         *    of adding the value of the second variable to the first
+         * 2. Create an echo statement that sends a line break to the screen.
+         **/
+        
+        /**
+         * 1. Create an echo statement to show the results
+         *    of subtracting the value of the second variable to the first
+         * 2. Create an echo statement that sends a line break to the screen.
+         **/
+        
+        /**
+         * 1. Create an echo statement to show the results
+         *    of multiplying the value of the second variable to the first
+         * 2. Create an echo statement that sends a line break to the screen.
+         **/
+        
+        /**
+         * 1. Create an echo statement to show the results
+         *    of dividing the value of the second variable to the first
+         * 2. Create an echo statement that sends a line break to the screen.
+         **/
+        
+        /**
+         * 1. Create an echo statement to show the
+         *    modulus (remainder) between the second variable to the first
+         * 2. Create an echo statement that sends a line break to the screen.
+         **/
 
     ?>
 </body>
--- Week-1/10-PHP-Assignment-Operators-Solution.php
+++ Week-1/10-PHP-Assignment-Operators-Solution.php
@@ -13,33 +13,51 @@
     $x = 3;
     $y = 12;
 
-    /*1. Create an echo statement to show the results of assigning the value of the second variable to the first
-        2. Create an echo statement that sends a line break to the screen.*/
+    /**
+     * 1. Create an echo statement to show the results
+     *    of assigning the value of the second variable to the first
+     * 2. Create an echo statement that sends a line break to the screen.
+     **/
     echo $x = $y;
     echo "<br>";
 
-    /*1. Create an echo statement to show the results of adding the value of the second variable to the first
-        2. Create an echo statement that sends a line break to the screen.*/
+    /**
+     * 1. Create an echo statement to show the results 
+     *    of adding the value of the second variable to the first
+     * 2. Create an echo statement that sends a line break to the screen.
+     **/
     echo $x += $y;
     echo "<br>";
 
-    /*1. Create an echo statement to show the results of subtracting the value of the second variable to the first
-        2. Create an echo statement that sends a line break to the screen.*/
+    /**
+     * 1. Create an echo statement to show the results
+     *    of subtracting the value of the second variable to the first
+     * 2. Create an echo statement that sends a line break to the screen.
+     **/
     echo $x -= $y;
     echo "<br>";
 
-    /*1. Create an echo statement to show the results of multiplying the value of the second variable to the first
-        2. Create an echo statement that sends a line break to the screen.*/
+    /**
+     * 1. Create an echo statement to show the results
+     *    of multiplying the value of the second variable to the first
+     * 2. Create an echo statement that sends a line break to the screen.
+     **/
     echo $x *= $y;
     echo "<br>";
 
-    /*1. Create an echo statement to show the results of dividing the value of the second variable to the first
-        2. Create an echo statement that sends a line break to the screen.*/
+    /**
+     * 1. Create an echo statement to show the results
+     *    of dividing the value of the second variable to the first
+     * 2. Create an echo statement that sends a line break to the screen.
+     **/
     echo $x /= $y;
     echo "<br>";
 
-    /*1. Create an echo statement to show the modulus (remainder) between the second variable to the first
-        2. Create an echo statement that sends a line break to the screen.*/
+    /**
+     * 1. Create an echo statement to show the
+     *    modulus (remainder) between the second variable to the first
+     * 2. Create an echo statement that sends a line break to the screen.
+     **/
     echo $x %= $y;
     echo "<br>";
 
