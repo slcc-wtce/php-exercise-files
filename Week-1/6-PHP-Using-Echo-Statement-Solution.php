78cf36a940cbe18936b5fbe098fc03573e56606e
slcc-wtce <64936479+slcc-wtce@users.noreply.github.com>
2020-10-13 15:32:16 -0600

    Update 6-PHP-Using-Echo-Statement-Solution.php

--- Week-1/6-PHP-Using-Echo-Statement-Solution.php
+++ Week-1/6-PHP-Using-Echo-Statement-Solution.php
@@ -9,13 +9,18 @@
 
 <body>
     <?php
-    /* 1. Create a date variable that will hold today's date (= date("M. d, Y"))
-    2. Create several echo statements that builds a basic webpage
-        and show the value of the date variable*/
-    $date = date("M. d, Y");
-    echo "<h2>Built with PHP!</h2>";
-    echo "<p>Building webpages with PHP is fun.</p>";
-    echo "Today is " . $date . ".";
+        /**
+		  * 1. Create a date variable that will hold 
+		  *    today's date (= date("M. d, Y"))
+		  * 2. Create several echo statements that 
+		  *    builds a basic webpage and shows the
+		  *    value of the date variable
+		**/
+    
+        $date = date("M. d, Y");
+        echo "<h2>Built with PHP!</h2>";
+        echo "<p>Building webpages with PHP is fun.</p>";
+        echo "Today is " . $date . ".";
 
     ?>
 </body>
