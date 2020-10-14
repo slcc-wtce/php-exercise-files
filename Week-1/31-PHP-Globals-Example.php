81675bc21d0eb9dc21c1514562038b4c821e1475
Spencer Lewis <spenc.lewis@gmail.com>
2020-10-14 16:54:34 -0600

    Adding exercise files

--- Week-1/31-PHP-Globals-Example.php
+++ Week-1/31-PHP-Globals-Example.php
@@ -17,65 +17,68 @@
 
     <h2>$GLOBALS Global</h2>
     <?php 
-    $x = 75; 
-    $y = 25;
-    function addition() { 
-            $GLOBALS['z'] = $GLOBALS['x'] + $GLOBALS['y']; 
-        }
-    
-    addition(); 
-    
-    echo "$x + $y = $z"; 
-    ?>
+        $x = 75; 
+        $y = 25;
+        function addition() { 
+                $GLOBALS['z'] = $GLOBALS['x'] + $GLOBALS['y']; 
+            }
+        
+        addition(); 
+        
+        echo "$x + $y = $z"; 
+        ?>
     
     
     <h2>$_REQUEST Global</h2>
     <form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
-    Name: <input type="text" name="fname">
+    Name: <input type="text" name="reqName">
     <input type="submit">
     </form>
     <?php
-        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        //Request Method is POST and the reqName field is populated
+        if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_REQUEST['reqName'])) {
             // collect value of input field
-            $name = $_REQUEST['fname'];
-            if (empty($name)) {
+            $rname = $_REQUEST['reqName'];
+            if (empty($rname)) {
                 echo "Name is empty";
             } else {
-                echo $name;
+                echo $rname;
             }
         }
     ?>
 
     <h2>$_GET Global</h2>
     <form method="get" action="<?php echo $_SERVER['PHP_SELF'];?>">
-    Name: <input type="text" name="fname">
+    Name: <input type="text" name="getName">
     <input type="submit">
     </form>
     <?php
-    if ($_SERVER["REQUEST_METHOD"] == "GET") {
-        // collect value of input field
-        $name = $_GET['fname'];
-        if (empty($name)) {
-            echo "Name is empty";
-        } else {
-            echo $name;
+        //Request Method is GET and the getName field is populated
+        if ($_SERVER["REQUEST_METHOD"] == "GET" && isset($_GET['getName'])) {
+            // collect value of input field
+            $gname = $_GET['getName'];
+            if (empty($gname)) {
+                echo "Name is empty";
+            } else {
+                echo $gname;
+            }
         }
-    }
     ?>
 
     <h2>$_POST Global</h2>
     <form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
-    Name: <input type="text" name="fname">
+    Name: <input type="text" name="postName">
     <input type="submit">
     </form>
     <?php
-        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        //Request Method is POST and the postName field is populated
+        if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['postName'])) {
             // collect value of input field
-            $name = $_POST['fname'];
-            if (empty($name)) {
+            $pname = $_POST['postName'];
+            if (empty($pname)) {
                 echo "Name is empty";
             } else {
-                echo $name;
+                echo $pname;
             }
         }
     ?>
