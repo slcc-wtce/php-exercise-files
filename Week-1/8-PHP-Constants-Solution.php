d34d42742c05ccf4fb4858984c7cb6dcb52b09a1
Spencer Lewis <spenc.lewis@gmail.com>
2020-10-14 16:43:12 -0600

    Adding exercise files

--- Week-1/8-PHP-Constants-Solution.php
+++ Week-1/8-PHP-Constants-Solution.php
@@ -9,30 +9,31 @@
 
 <body>
     <?php
-    /******************************************************
-     * Create a constant called GREETING and assign it a 
-     * basic HTML paragraph
-    ******************************************************/
-    define("GREETING", "<p>Welcome to SLCC!</p>");
+        /******************************************************
+         * Create a constant called GREETING and assign it a 
+         * basic HTML paragraph
+        ******************************************************/
+        define("GREETING", "<p>Welcome to SLCC!</p>");
 
-    /******************************************************
-     * Create an echo statement to send the content of 
-     * the GREETING constant to the screen
-    ******************************************************/
-    echo GREETING;
+        /******************************************************
+         * Create an echo statement to send the content of 
+         * the GREETING constant to the screen
+        ******************************************************/
+        echo GREETING;
 
-    /******************************************************
-     * Create a constant called tagLine 
-     * and assign it a basic HTML paragraph
-     * Set it to work regardless of letter case.
-    ******************************************************/
-    define("tagLine", "<p>Working with PHP is rewarding!</p>", true);
+        /******************************************************
+         * Create a constant called tagLine 
+         * and assign it a basic HTML paragraph
+         * Set it to work regardless of letter case.
+        ******************************************************/
+        define("tagLine", "<p>Working with PHP is rewarding!</p>", true);
 
-    /******************************************************
-     * Create an echo statement to send the content
-     * of the tagLine constant to the screen
-    ******************************************************/
-    echo tagLine;
+        /******************************************************
+         * Create an echo statement to send the content
+         * of the tagLine constant to the screen
+        ******************************************************/
+        echo tagLine;
+
     ?>
 </body>
 
