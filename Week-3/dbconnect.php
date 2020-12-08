d5a44ad7a39fd5481ceb31249242fbe2b3b2aa97
Spencer Lewis <spenc.lewis@gmail.com>
2020-12-07 17:02:10 -0700

    Adding exercise files

---
+++ Week-3/dbconnect.php
@@ -0,0 +1,23 @@
+<?php
+    $hostname = "php-mysql-exercisedb.slccwebdev.com";
+    $username = "phpmysqlexercise";
+    $password = "mysqlexercise";
+    $databasename = "php_mysql_exercisedb";
+
+    try {
+        //Create new PDO Object with connection parameters
+        $conn = new PDO("mysql:host=$hostname;dbname=$databasename",$username, $password);
+        
+        //Set PDO error mode to exception
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
+        
+        //Send success message to screen
+        echo "Database Connection Successful!";
+
+
+    } catch (PDOException $error) {
+
+        //Return error code if one is created
+        echo "Connection Failed: " . getMessage($error);
+    }
+?>
